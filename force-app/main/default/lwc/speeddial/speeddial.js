643194d0e655177c863626270ea20caffd758c4d
Chloe Echikson <rubyquin@gmail.com>
2021-03-31 11:58:27 -0400

    Implemented basic speed dial functionality and simple implementation

--- force-app/main/default/lwc/speeddial/speeddial.js
+++ force-app/main/default/lwc/speeddial/speeddial.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class Speeddial extends LightningElement {}
+export default class Speeddial extends LightningElement {
+  @api options = [];
+  showOptions;
+
+  handleShowOptions() {
+    const dial = this.template.querySelector(".dial");
+    dial.classList.add("rotate");
+    this.showOptions = true;
+  }
+
+  handleHideOptions() {
+    const dial = this.template.querySelector(".dial");
+    dial.classList.remove("rotate");
+    this.showOptions = false;
+  }
+
+  handleToggleOptions() {
+    this.showOptions = !this.showOptions;
+
+    const dial = this.template.querySelector(".dial");
+    if (this.showOptions) {
+      dial.classList.add("rotate");
+    } else {
+      dial.classList.remove("rotate");
+    }
+  }
+
+  handleClick(event) {
+    const evt = new CustomEvent("buttonclick", {
+      detail: event.target.dataset.name
+    });
+    this.dispatchEvent(evt);
+  }
+}
